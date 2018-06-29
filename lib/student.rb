9c24afc75958fc7b44bd3eeca7cf572db06b47fb
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-06-29 00:50:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,9 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 9
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.students_below_12th_grade
